539f1ad7b81a05cdab1cc04367879aef55fe3757
Ricardo <shooghr@hotmail.com>
2015-07-13 09:07:57 -0300

    Pesquisa Implemantada

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -22,27 +22,45 @@ class CommunityController < ApplicationController
   def browse
   end
 
+  # @Nova Implementação do Método Search
   def search
-	  	@title = "Search RailsSpace"
-	  	if params[:q]
-	  		query = params[:q]
+  	@title = "Search RailSpace"
+  	if params[:q]
+  		@initial = params[:q]
+  		specs = Spec.where("last_name LIKE ? or first_name LIKE ?", '%'+@initial+'%', '%'+@initial+'%').order(:last_name, :first_name).paginate(page: params[:page], per_page: 10)
+  		@users = specs.collect { |spec| spec.user }
 
-	  		#First find the user hits...
-	  		@users = User.find_with_ferret(params[:q])
-	  		#....then the subhits.
-	  		specs = Spec.find_with_ferret(query, :limit => 10)
-			faqs = Faq.find_with_ferret(query, :limit => :all)
+  		@pages = specs
+  	end
+  end
 
+#  @Antiga Implementação do Método Search ('gem ferret' e 'gem acts_as_ferret' não funcionaram)
+#
+#  def search
+#	  	@title = "Search RailsSpace"
+#	  	if params[:q]
+#	  		query = params[:q]
+#
+#	  		#First find the user hits...
+#	  		@users = User.find_with_ferret(params[:q])
+#	  		#....then the subhits.
+#	  		specs = Spec.find_with_ferret(query, :limit => 10)
+#			faqs = Faq.find_with_ferret(query, :limit => :all)
+#
+#
+#			# Now combine into one list of distinct users sorted by last name.
+#			hits = specs + faqs
+#			@users.concat(hits.collect{ |hit| hit.user }).uniq!
+#
+#			# Sort by last name (requires a spec for each user).
+#			@users.each { |user| user.spec ||= Spec.new }
+#			@users = @users.sort_by { |user| user.spec.last_name }
+#	  end
+#  end
 
-			# Now combine into one list of distinct users sorted by last name.
-			hits = specs + faqs
-			@users.concat(hits.collect{ |hit| hit.user }).uniq!
+	private
 
-			# Sort by last name (requires a spec for each user).
-			@users.each { |user| user.spec ||= Spec.new }
-			@users = @users.sort_by { |user| user.spec.last_name }
-	  end
-   end
-
+	def improviment_search
+	end
 
 end
