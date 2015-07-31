dae9b99e9e670ddb0aeea97408b2105e48c112a8
Ricardo <shooghr@hotmail.com>
2015-07-31 14:36:48 -0300

    Envio de messagem de email finalizado

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,4 +1,6 @@
 class EmailController < ApplicationController
+  include ProfileHelper
+  before_filter :protect, :only => [:correspond]
 
   def remind
   	@title = "Mail me my login information"
@@ -11,6 +13,26 @@ class EmailController < ApplicationController
   			redirect_to :action => "index", :controller => "site"
   		else
   			flash[:notice] = "There is no user with that email address."
+  		end
+  	end
+  end
+
+  def correspond
+  	user = User.find(session[:user_id])
+  	recipient = User.find_by_screen_name(params[:id])
+  	@title = "Email #{recipient.name}"
+  	if param_posted?(:message)
+  		@message = Message.new(params[:message])
+  		if @message.valid?
+	  		UserMailer.message_email(
+	  			:user => user,
+	  			:recipient => recipient,
+	  			:message => @message,
+	  			:user_url => profile_for(user),
+	  			:reply_url => url_for(:action => "correspond", :id => user.screen_name)
+	  		).deliver_now
+	  		flash[:notice] = "Email sent."
+	  		redirect_to profile_for(recipient)
   		end
   	end
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,9 @@ class UserMailer < ActionMailer::Base
   	@user = user
   	mail(:to => user.email, :subject => "Your login information at RailsSpace.com"  )
   end
+
+  def message_email(mail)
+  	@mail = mail
+  	mail(:to => mail[:recipient].email, :subject => mail[:message].subject, :body => mail)
+  end
 end
